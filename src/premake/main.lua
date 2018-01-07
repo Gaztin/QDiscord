cfb9721d591fa620cead2882bd067840cb421a15
Sebastian Kylander <sebastian@kylander.me>
2018-01-07 16:32:40 +0100

    Add main premake script

---
+++ src/premake/main.lua
@@ -0,0 +1,87 @@
+require 'extensions/premake-qt/qt'
+
+local qtdir_x86 = io.readfile('../../tmp/.qtdir_x86')
+local qtdir_x64 = io.readfile('../../tmp/.qtdir_x64')
+
+workspace ('QDiscord')
+	location ('../')
+	configurations {
+		'Debug',
+		'Release',
+	}
+	platforms {
+		'x86',
+		'x64',
+	}
+
+project ('QDiscordCore')
+	premake.extensions.qt.enable()
+	location ('../projects/')
+	kind ('StaticLib')
+	qtgenerateddir ('core/GeneratedFiles/')
+	qtprefix ('Qt5')
+	files {
+		'../core/**.h',
+		'../core/**.cpp',
+	}
+	includedirs {
+		'../core/',
+	}
+	qtmodules {
+		'core',
+	}
+
+	filter {'platforms:x86'}
+		objdir ('../../bin/x86/obj/')
+		qtpath (qtdir_x86)
+		targetdir ('../../bin/x86/')
+
+	filter {'platforms:x64'}
+		objdir ('../../bin/x64/obj/')
+		qtpath (qtdir_x64)
+		targetdir ('../../bin/x64/')
+
+	filter {}
+
+project ('Demo')
+	premake.extensions.qt.enable()
+	dependson ('QDiscordCore')
+	location ('../projects/')
+	kind ('ConsoleApp')
+	qtgenerateddir ('demo/GeneratedFiles/')
+	qtprefix ('Qt5')
+	files {
+		'../demo/**.h',
+		'../demo/**.cpp',
+		'../demo/**.ui',
+		'../demo/**.qrc',
+	}
+	includedirs {
+		'../core/',
+		'../demo/',
+	}
+	links {
+		'QDiscordCore',
+	}
+	qtmodules {
+		'core',
+		'gui',
+		'widgets',
+	}
+
+	filter {'configurations:Debug'}
+		qtsuffix ('d')
+
+	filter {'platforms:x86'}
+		debugdir ('../../bin/x86/')
+		objdir ('../../bin/x86/obj/')
+		qtpath (qtdir_x86)
+		targetdir ('../../bin/x86/')
+
+	filter {'platforms:x64'}
+		debugdir ('../../bin/x64/')
+		objdir ('../../bin/x64/obj/')
+		qtpath (qtdir_x64)
+		targetdir ('../../bin/x64/')
+
+	filter {}
