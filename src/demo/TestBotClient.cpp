3b9c480ab26b77015032ddd441db3e9b22929fe6
Sebastian Kylander <sebastian@kylander.me>
2018-01-12 12:52:57 +0100

    Update demo
    * Print author usernames when echoing messages
    * Add '!img' command that takes emoji arguments and sends back the
      image url

--- src/demo/TestBotClient.cpp
+++ src/demo/TestBotClient.cpp
@@ -22,8 +22,39 @@ void TestBotClient::onMessageCreate(const Discord::Message& message)
 	{
 		sendMessage(message.channel_id, "This is a test message");
 	}
+	else if (message.content.startsWith("!img "))
+	{
+		QStringList args = message.content.split(' ');
+		args.pop_front();
+
+		QStringList new_messages;
+		while (!args.empty())
+		{
+			// Remove start and end tags ('<' and '>')
+			QString emoji = args[0];
+			emoji.remove(0, 1);
+			emoji.remove(emoji.length() - 1, 1);
+
+			// Parse the emoji ID
+			QStringList split_emoji = emoji.split(':');
+			if (split_emoji.size() == 3)
+			{
+				new_messages.append(QString(
+					"https://cdn.discordapp.com/emojis/%1.png").arg(
+						split_emoji[2]));
+			}
+
+			args.pop_front();
+		}
+
+		if (!new_messages.empty())
+		{
+			sendMessage(message.channel_id, new_messages.join('\n'));
+		}
+	}
 
 #ifdef QT_DEBUG
-	qDebug("onMessageCreate (%s)", qPrintable(message.content));
+	qDebug("[%s]: %s", qPrintable(message.author.username),
+		qPrintable(message.content));
 #endif
 }
