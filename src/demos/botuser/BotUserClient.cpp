4f922e8ef39fb225e8296da2e23b81fc388ef346
Sebastian Kylander <sebastian@kylander.me>
2018-04-07 22:17:40 +0200

    Add channel and message browsing

--- src/demos/botuser/BotUserClient.cpp
+++ src/demos/botuser/BotUserClient.cpp
@@ -22,7 +22,8 @@ void BotUserClient::onReady(const Discord::User& user,
 			[&](const Discord::Guild& g)
 		{
 			QListWidgetItem* item = new QListWidgetItem;
-			window_layout_ref_.list_widget->addItem(item);
+			item->setData(Qt::UserRole, g.id());
+			window_layout_ref_.list_widget_guilds->addItem(item);
 
 			getGuildIconPixmap(g,
 				Discord::Client::IconImageSupportedExtension::PNG).then(
@@ -40,4 +41,29 @@ void BotUserClient::onReady(const Discord::User& user,
 
 void BotUserClient::onMessageCreate(const Discord::Message& message)
 {
+	QList<QListWidgetItem*> selected_guild_items =
+		window_layout_ref_.list_widget_guilds->selectedItems();
+	if (selected_guild_items.length() != 1)
+		return;
+
+	QList<QListWidgetItem*> selected_channel_items =
+		window_layout_ref_.list_widget_channels->selectedItems();
+	if (selected_channel_items.length() != 1)
+		return;
+
+	QListWidgetItem* guild_item = selected_guild_items[0];
+	QListWidgetItem* channel_item = selected_channel_items[0];
+	getChannel(message.channelId()).then(
+		[this, channel_item, guild_item, message](const Discord::Channel& c)
+	{
+		if (c.guildId() != guild_item->data(Qt::UserRole).toULongLong())
+			return;
+		if (c.id() != channel_item->data(Qt::UserRole).toULongLong())
+			return;
+
+		QListWidgetItem* item = new QListWidgetItem;
+		item->setData(Qt::UserRole, message.id());
+		item->setText(message.author().username() + ": " + message.content());
+		window_layout_ref_.list_widget_messages->addItem(item);
+	});
 }
--- src/demos/botuser/MainWindow.cpp
+++ src/demos/botuser/MainWindow.cpp
@@ -10,6 +10,57 @@ MainWindow::MainWindow(QWidget* parent)
 
 	connect(ui_.action_about_qt, &QAction::triggered, &QApplication::aboutQt);
 	connect(ui_.action_log_in, &QAction::triggered, this, &MainWindow::logIn);
+
+	connect(ui_.list_widget_guilds, &QListWidget::itemSelectionChanged,
+		[this]
+	{
+		ui_.list_widget_channels->clear();
+		ui_.list_widget_messages->clear();
+
+		QList<QListWidgetItem*> selected_items =
+			ui_.list_widget_guilds->selectedItems();
+		if (selected_items.length() == 1)
+		{
+			snowflake_t guild_id = selected_items[0]->data(
+				Qt::UserRole).toULongLong();
+			discord_client_.getGuildChannels(guild_id).then(
+				[this](const QList<Discord::Channel>& cs)
+			{
+				Q_FOREACH(const Discord::Channel& c, cs)
+				{
+					QListWidgetItem* item = new QListWidgetItem;
+					item->setData(Qt::UserRole, c.id());
+					item->setText("#" + c.name());
+					ui_.list_widget_channels->addItem(item);
+				}
+			});
+		}
+	});
+
+	connect(ui_.list_widget_channels, &QListWidget::itemSelectionChanged,
+		[this]
+	{
+		ui_.list_widget_messages->clear();
+
+		QList<QListWidgetItem*> selected_items =
+			ui_.list_widget_channels->selectedItems();
+		if (selected_items.length() == 1)
+		{
+			snowflake_t channel_id = selected_items[0]->data(
+				Qt::UserRole).toULongLong();
+			discord_client_.getChannelMessages(channel_id).then(
+				[this](const QList<Discord::Message>& ms)
+			{
+				Q_FOREACH(const Discord::Message& m, ms)
+				{
+					QListWidgetItem* item = new QListWidgetItem;
+					item->setData(Qt::UserRole, m.id());
+					item->setText(m.author().username() + ": " + m.content());
+					ui_.list_widget_messages->addItem(item);
+				}
+			});
+		}
+	});
 }
 
 void MainWindow::logIn()
